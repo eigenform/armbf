95914aefc690f3117086400f6c0c66a2b1719846
meta <hosaka-corp@users.noreply.github.com>
2020-06-16 00:37:06 -0500

    armbf: more generic lookup table types?

--- bf/armbf/src/inst.rs
+++ bf/armbf/src/inst.rs
@@ -4,6 +4,34 @@ use crate::newtype::*;
 use crate::fields::*;
 use armbf_prim::*;
 
+use std::marker::Copy;
+
+/// An ARMv5 lookup table.
+pub struct ArmLut<T: ArmLutEntry> { pub data: [T; 0x1000] }
+
+/// Implemented on all types store-able by some ArmLut.
+pub trait ArmLutEntry { 
+    /// A map from ArmInst to some ArmLutEntry.
+    fn from_inst(inst: ArmInst) -> Self;
+}
+
+/// Creates a new ArmLookupTable for some T.
+pub fn CreateArmLut<T: ArmLutEntry + Copy>(default_entry: T) -> ArmLut<T> {
+    let mut lut = ArmLut {
+        data: [default_entry; 0x1000],
+    };
+
+    // The details of how to obtain an entry [of type T] are left to the user.
+    for i in 0..0x1000 {
+        let inst: u32 = ((i & 0x0ff0) << 16) | ((i & 0x000f) << 4);
+        lut.data[i as usize] = T::from_inst(ArmInst::from_u32(inst));
+    }
+    lut
+}
+
+
+
+
 #[derive(Debug)]
 pub enum ArmInst {
     None,
